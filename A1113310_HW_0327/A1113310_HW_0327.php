53c39245cec3774a9a707cd2a3ed8b66f789bc53
lucioleolin <125644692+lucioleolin@users.noreply.github.com>
2026-03-26 20:53:17 +0800

    Add files via upload

---
+++ A1113310_HW_0327/A1113310_HW_0327.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+    <title>Camp Registration</title>
+</head>
+
+<body bgcolor="lightgreen">
+
+<center>
+    <h2><font color="darkgreen">2026 Summer Camp</font></h2>
+    <p>Fill in the form below to join our camp!</p>
+</center>
+
+<form method="post">
+
+<table border="1" align="center" cellpadding="10" bgcolor="white">
+
+<tr>
+    <td colspan="2"><b>Personal Information</b></td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Name:<br>
+        <input type="text" name="name">
+    </td>
+</tr>
+
+<tr>
+    <td>
+        Gender:<br>
+        <input type="radio" name="gender" value="Male"> Male<br>
+        <input type="radio" name="gender" value="Female"> Female
+    </td>
+
+    <td>
+        Birthday:<br>
+        <input type="date" name="birthday">
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Email:<br>
+        <input type="text" name="email">
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Phone:<br>
+        <input type="text" name="phone">
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2"><b>Camp Details</b></td>
+</tr>
+
+<tr>
+    <td>
+        Location:<br>
+        <select name="location">
+            <option>Taipei</option>
+            <option>Taichung</option>
+            <option>Kaohsiung</option>
+        </select>
+    </td>
+
+    <td>
+        Session:<br>
+        <select name="session">
+            <option>Week 1</option>
+            <option>Week 2</option>
+        </select>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Activities:<br>
+        <input type="checkbox"> Sports
+        <input type="checkbox"> Art
+        <input type="checkbox"> Games
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Message:<br>
+        <textarea rows="4" cols="35"></textarea>
+    </td>
+</tr>
+
+<tr>
+    <td align="center">
+        <input type="submit" value="Send">
+    </td>
+    <td align="center">
+        <input type="reset" value="Clear">
+    </td>
+</tr>
+
+</table>
+
+</form>
+
+</body>
+</html>
